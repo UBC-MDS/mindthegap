33033c19d3980d9687339ae1fe100e013cb8851d
James Kim <james.ktkim@gmail.com>
2022-03-02 18:41:48 -0800

    updating the year filter to slider and bar chart to be dynamic

--- src/app.py
+++ src/app.py
@@ -76,17 +76,29 @@ app.layout = html.Div([
     
     dcc.RadioItems(id='metric', value='life_expectancy',
         options=[{'label': v, 'value': k} for k, v in metrics.items()]),
-    html.Br(),
-    'Year',
-    dcc.Dropdown(id='yr', 
-        options=[
-                {'label': 1990, 'value': 1990},
-                {'label': 2000, 'value': 2000},
-                {'label': 2012, 'value': 2012},
-                {'label': 2015, 'value': 2015}
-                ],
-            value=2012) ,
-    html.Br(),
+
+    # adding the slider instead to filter year
+    html.H3('Year'),
+    html.P('The user can slide over the years to filter the year'),
+    dcc.Slider(
+        min=1970,
+        max=2010,
+        step=5,
+        value=2010,
+        id='yr',
+        marks={i: str(i) for i in range(1970, 2015, 5)},
+        ),
+    # html.Br(),
+    # 'Year',
+    # dcc.Dropdown(id='yr', 
+    #     options=[
+    #             {'label': 1990, 'value': 1990},
+    #             {'label': 2000, 'value': 2000},
+    #             {'label': 2012, 'value': 2012},
+    #             {'label': 2015, 'value': 2015}
+    #             ],
+    #         value=2012) ,
+    # html.Br(),
     dbc.Row(
         [
             dbc.Col(
@@ -267,9 +279,10 @@ def plot_bubble_chart(yr):
 # Set up callbacks/backend
 @app.callback(
     Output('barchart', 'srcDoc'),
+    Input("metric", "value"),
     Input('yr', 'value'))
 
-def plot_country(yr):
+def plot_country(metric, yr):
 
     """Create a bar chart for top 10 countries in terms of life expectancy.
 
@@ -284,16 +297,17 @@ def plot_country(yr):
         The bar chart.
     """
 
-    df = filter_year(yr)
+    data = filter_data(None, None, None, yr)
 
     country = (
-        alt.Chart(df, title='Top 10 Countries').mark_bar().encode(
+        alt.Chart(data, title=f"{metrics[metric]} - Top 10 Country for Year {yr}")
+        .mark_bar().encode(
             y=alt.Y('country', sort='-x', title='Country'),
-            x=alt.X('life_expectancy', title='Life Expectancy'),
-            color=alt.Color('life_expectancy')
+            x=alt.X(metric, title=metrics[metric]),
+            color=alt.Color(metric + ":Q", title=metrics[metric])
         ).transform_window(
             rank='rank(life_expectancy)',
-            sort=[alt.SortField('life_expectancy', order='descending')]
+            sort=[alt.SortField(metric, order='descending')]
         ).transform_filter(
             (alt.datum.rank < 10))
     )
